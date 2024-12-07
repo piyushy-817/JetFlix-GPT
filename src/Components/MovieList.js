daa9002c43615e60d469f0c9f900b7307d6c33cc
Piyush Yadav <piyushyadav970@gmail.com>
2024-12-07 22:44:40 +0530

    11

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MovieCard from './MovieCard'
-import { Link, useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const MovieList = ({title , movies}) => {
  
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../utils/userSlice"
 import moviesReducer from "./movieSlice"
+import SingleMovieReducer from "./singleMovieSlice"
 
 const appStore = configureStore({
     reducer:{
        user : userReducer,
        movies : moviesReducer,
+       singleMovie : SingleMovieReducer,
     }
 })
 
